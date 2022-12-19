c7e60e816d4e968715a4b364fb51bad389aba8ed
Savarimuthu Sagayaraj <asavarimuthu@gmail.com>
2022-12-19 20:40:24 +0000

    GIT Comment

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("businesspartnerapp.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
